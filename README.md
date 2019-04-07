ae6fd597f0fcaf9324f6cfaa65fccfb4a3082461
luoxiaojun <luoxiaojun@dodoca.com>
2019-04-07 16:22:19 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Yii2 Zipkin Demo
+
+> ## Usage
+> 1. ```git clone git@github.com:luoxiaojun1992/yii2-zipkin-demo.git && cd yii2-zipkin-demo```
+> 2. ```composer install -vvv```
+> 3. ```./yii serve```
+> 5. ```curl 'http://127.0.0.1/'```
---
+++ config/web.php
@@ -0,0 +1,96 @@
+<?php
+
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/db.php';
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'iVn3pWsT4Go1ovUVKP0tDcY2MuHDLX_L',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+        'zipkin' => [
+            'class' => \Lxj\Yii2\Zipkin\Tracer::class,
+            'serviceName' => 'yii2-zipkin-demo',
+            'endpointUrl' => 'http://localhost:9411/api/v2/spans',
+            'sampleRate' => 1,
+            'bodySize' => 500,
+            'curlTimeout' => 1,
+            'reportType' => 'http',
+            'redisOptions' => [
+                'queue_name' => 'queue:zipkin:span',
+                'connection' => 'zipkinRedis',
+            ],
+        ],
+        'zipkinRedis' =>[
+            'class' => 'yii\redis\Connection',
+            'hostname' => 'localhost',
+            'port' => 6379,
+            'database' => 0,
+            'connectionTimeout' => 0.01,
+            'dataTimeout' => 0.01,
+        ],
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        */
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+}
+
+return $config;
